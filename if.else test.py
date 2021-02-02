d23e07da1174d0081dc1c4032f410203adb1cb2e
Eduardo <78316076+eduardodelarge@users.noreply.github.com>
2021-02-02 17:05:49 -0300

    Add files via upload

---
+++ if.else test.py
@@ -0,0 +1,8 @@
+answer = input("Is Python an interpreted language? Yes or No >> ").lower()
+
+if answer == "yes" :
+    print("You have cleared the test.")
+else :
+    print("You have failed the test.")
+
+print("Thanks!")
